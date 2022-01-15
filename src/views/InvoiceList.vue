ff5f6c58dc84fee957e1baf0cd3122a505a5747d
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2022-01-15 13:58:48 +0800

    使用momentjs格式化時間，列出發票重點資訊

--- src/views/InvoiceList.vue
+++ src/views/InvoiceList.vue
@@ -1,27 +1,34 @@
 <template>
-  <div class="container mx-auto">
+  <div id="invoice-list" class="container mx-auto">
     <h4>110年12月</h4>
     <p>共{{ invoices.length }}張發票，總金額{{ totalPrice }}元</p>
-    <div
-      class="row"
-      v-for="(invoice, index) in invoices"
-      :key="index"
-    >
-      <div class="col-1">載具</div>
-      <div class="col-6">{{ invoice.invNum }}</div>
-      <div class="col-5">{{ invoice.amount }}元</div>
+    <div class="row" v-for="(invoice, index) in invoices" :key="index">
+      <div class="col-1 d-flex flex-column">
+        <div class="date">{{ toMMDD(invoice.time) }}</div>
+        <div class="tag">載具</div>
+      </div>
+      <div class="col-6 text-start">
+        <div class="seller">
+          {{ invoice.sellerName ? invoice.sellerName : invoice.invNum }}
+        </div>
+        <div class="first-item">
+          {{ invoice.details ? invoice.details[0].description : '無店家資料' }}
+        </div>
+      </div>
+      <div class="col-5">{{ invoice.amount ? invoice.amount : '--' }}元</div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
 export default {
   name: 'InvoiceList',
   data() {
-    return { 
-      invoices: [], 
-      }
+    return {
+      invoices: [],
+    }
   },
   mounted() {
     axios
@@ -33,6 +40,15 @@ export default {
       .catch((error) => {
         console.log(error)
       })
+  },
+  methods: {
+    toMMDD(date) {
+      let YearMonthDay = moment(date).format('YYYY/MM/DD')
+      let Month = moment(YearMonthDay).format('M')
+      let Day = moment(YearMonthDay).format('D')
+      console.log('YearMonthDay', YearMonthDay)
+      return `${Month}/${Day}`
+    },
   },
   computed: {
     totalPrice() {
